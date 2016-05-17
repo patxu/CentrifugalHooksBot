dd8cdd5985307f6beb55cd40b8d5f975e3d1bb60
patxu <patrick.s.xu.17@dartmouth.edu>
2016-05-17 00:51:26 -0400

    add several event hooks

--- web.js
+++ web.js
@@ -5,10 +5,40 @@ console.log("centrifugal-hooks bot starting up");
 // import modules
 var express = require("express");
 var app = express();
-var rtmClient = require("@slack/client").RtmClient; // real-time messaging
+var slackClient = require("@slack/client").RtmClient; // real-time messaging
 
 // setup Slack client
 var token = process.env.SLACK_BOT_TOKEN, // Add a bot at      https://my.slack.com/services/new/bot and copy the token here.
   autoReconnect = true,
   autoMark = true;
-var rtm = new rtmClient(token, autoReconnect, autoMark);
+var slack = new slackClient(token, autoReconnect, autoMark);
+
+
+/**
+ * messege event- https://api.slack.com/events/message
+ */
+slack.on('message', onMessage(message));
+
+
+/**
+ * team_join event- https://api.slack.com/events/team_join
+ */
+slack.on('team_join', onTeamJoin());
+
+function onMessage(message) {
+  var type = message.type,
+      channel = slack.getChannelGroupOrDMByID(message.channel),
+      user = slack.getUserByID(message.user),
+      time = message.ts,
+      text = (message.text) ? message.text : "",
+      response = '';
+}
+
+/**
+ * onTeamJoin - description
+ *
+ * @returns {type}  description
+ */
+function onTeamJoin(team_join) {
+
+}
