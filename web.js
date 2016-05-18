f8deab854b833eea0fdd3a9006aab79218c1dd0d
patxu <patrick.s.xu.17@dartmouth.edu>
2016-05-17 23:47:43 -0400

    welcome bot sends new members a message

--- web.js
+++ web.js
@@ -1,44 +1,98 @@
 // Pat Xu
 
-console.log("centrifugal-hooks bot starting up");
+console.log('Centrifugal Hooks bot starting up');
 
 // import modules
-var express = require("express");
+var express = require('express');
 var app = express();
-var slackClient = require("@slack/client").RtmClient; // real-time messaging
+var slackClient = require('@slack/client').RtmClient; // real-time messaging
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var MemoryDataStore = require('@slack/client').MemoryDataStore;
 
 // setup Slack client
-var token = process.env.SLACK_BOT_TOKEN, // Add a bot at      https://my.slack.com/services/new/bot and copy the token here.
-  autoReconnect = true,
-  autoMark = true;
-var slack = new slackClient(token, autoReconnect, autoMark);
+var token = process.env.SLACK_BOT_TOKEN;
+var slack = new slackClient(token, {
+  logLevel: 'error',
+  // Initialise a data store for our client, this will load additional helper functions for the storing and retrieval of data
+  dataStore: new MemoryDataStore(),
+  // Boolean indicating whether Slack should automatically reconnect after an error response
+  autoReconnect: true,
+  // Boolean indicating whether each message should be marked as read or not after it is processed
+  autoMark: true
+});
+slack.start();
 
-
-/**
- * messege event- https://api.slack.com/events/message
- */
-slack.on('message', onMessage(message));
-
-
-/**
- * team_join event- https://api.slack.com/events/team_join
- */
-slack.on('team_join', onTeamJoin());
-
-function onMessage(message) {
-  var type = message.type,
-      channel = slack.getChannelGroupOrDMByID(message.channel),
-      user = slack.getUserByID(message.user),
-      time = message.ts,
-      text = (message.text) ? message.text : "",
-      response = '';
-}
+var general; // #general channel
 
 /**
- * onTeamJoin - description
+ * log self and team name
+ * Client events: https://github.com/slackhq/node-slack-sdk/blob/5c8d3bdbcc38a71ca45d372444ce5f79178de557/lib/clients/events/client.js
  *
- * @returns {type}  description
+ * @param  {type} startData description
+ * @returns {type} description
  */
-function onTeamJoin(team_join) {
+slack.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (startData) {
+  console.log('Logged in as @%s of team %s, but not yet connected to a channel', startData.self.name, startData.team.name);
+});
 
-}
+/**
+  * log channels
+  *
+  * @param  {type} CLIENT_EVENTS.RTM.RTM_CONNECTION_OPEN
+  * @param  {type}
+  * @returns {type} description
+ */
+slack.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
+  var channels = Object.keys(slack.dataStore.channels)
+    .filter(function (c) {
+      return slack.dataStore.getChannelById(c).is_member;
+    })
+    .map(function(c) {
+      return slack.dataStore.getChannelById(c).name;
+    });
+
+  console.log('Channels: %s', channels.join(', '));
+
+
+  general = slack.dataStore.getChannelByName('general');
+  if (general !== null) {
+    general = '<#' + general.id + '>';
+  } else {
+    general = '#general';
+  }
+});
+
+/**
+  * messege event- https://api.slack.com/events/message
+  *
+  * @param  {type} RTM_EVENTS.MESSAGE description
+  * @param  {type} function(message   description
+  * @returns {type}                    description
+  */
+slack.on(RTM_EVENTS.MESSAGE, function(message){
+  var type = message.type,
+      channel = message.channel,
+      user = message.user,
+      time = message.ts,
+      text = message.text ? message.text : "",
+      response = '';
+  console.log('received %s', text);
+});
+
+
+/**
+  * team_join event- https://api.slack.com/events/team_join
+  *
+  * @param  {type} team_join description
+  * @returns {type} description
+  */
+
+slack.on(RTM_EVENTS.TEAM_JOIN, function onTeamJoin(team_join) {
+  var user = slack.dataStore.getUserById(team_join.user.id);
+  console.log('%s just joined the team!', user.name);
+
+  var dm = slack.dataStore.getDMByName(user.name);
+
+  slack.sendMessage('Hey ' + user.name + ', welcome to the Centrifugal Hooks channel! How are you doing? I\'m your friendly neighborhood brood leader and I\'m excited you\'re here! You can chat me but I can\'t do much yet– I\'m working on it... In the meantime, head over to ' + general + ' and say hi! Also go ahead and update your profile picture to your favorite icon from http://eu.battle.net/sc2/en/game/unit/ Happy kiting!', dm.id);
+});
